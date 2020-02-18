a71d703f1f56b6e296ff93080f3804d15051fba9
Kalina Balabnova <kalina.balabanova@bgosoftware.com>
2020-02-18 16:03:48 +0200

    first component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Input from './components/Input';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      My easy money app
+      </div>
+
+   <Input />
+    </div>
+  );
+}
+
+export default App;
